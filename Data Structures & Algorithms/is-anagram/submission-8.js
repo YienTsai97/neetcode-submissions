ca49f8c3bcd29a5486e2049767c9103bc1205fff
Yi-En Tsai <168599034+YienTsai97@users.noreply.github.com>
2026-09-20 22:55:51 +0800

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const map = new Map()
+
+        for(let c of s){
+            map.set(c, (map.get(c) || 0) + 1) 
+        }
+        for(let c of t){
+            map.set(c, (map.get(c) || 0) - 1)
+        }
+
+        let res = [...map.values()]
+        // return !res.some((num) => num !== 0)
+        return res.every((num) => num === 0)
+    } 
+}
